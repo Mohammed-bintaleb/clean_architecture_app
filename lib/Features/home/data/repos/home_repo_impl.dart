91b75ad2b14fac90189a2466bcbd296de719f311
Mohammed-bintaleb <engineerbintaleb@gmail.com>
2025-07-06 14:51:43 +0300

    Fetch newest books home repo impl

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -28,8 +28,17 @@ class HomeRrpoImpl extends HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() {
-    // TODO: implement fetchNewestBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async {
+    try {
+      var booksList = homeLocalDataSource.fetchNewestBooks();
+      if (booksList.isNotEmpty) {
+        return right(booksList);
+      }
+      var books = await homeRemoteDataSource.fetchNewestBooks();
+
+      return right(books);
+    } catch (e) {
+      return left(Failure());
+    }
   }
 }
