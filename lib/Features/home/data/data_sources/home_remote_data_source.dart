0090e55e2f7abf4e9a3065d09aa1f38f495e0c87
Mohammed-bintaleb <engineerbintaleb@gmail.com>
2025-07-06 11:59:46 +0300

    Create home remote data source

---
+++ lib/Features/home/data/data_sources/home_remote_data_source.dart
@@ -0,0 +1,8 @@
+import '../../domain/entities/book_entity.dart';
+
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntity>> fetchFeatureBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
+}
+
+class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {}
