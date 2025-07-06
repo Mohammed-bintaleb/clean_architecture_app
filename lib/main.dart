96315670141ee7138c1b59794f411c101c70d5e9
Mohammed-bintaleb <engineerbintaleb@gmail.com>
2025-07-06 17:17:01 +0300

    get it part 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:bookly/Features/home/data/repos/home_repo_impl.dart';
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/Features/home/domain/use_cases/fetch_feature_books_use_case.dart';
+import 'package:bookly/Features/home/domain/use_cases/fetch_newest_books_use_case.dart';
 import 'package:bookly/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly/Features/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
@@ -7,10 +11,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/adapters.dart';
 
+import 'core/functions/setup_service_locator.dart';
+
 void main() async {
+  setupServiceLocator();
   await Hive.initFlutter();
   Hive.registerAdapter(BookEntityAdapter());
-
   await Hive.openBox<BookEntity>(kFeatureBox);
   await Hive.openBox<BookEntity>(kNewestBox);
   runApp(const Bookly());
@@ -24,7 +30,11 @@ class Bookly extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(
-            create: (_) => FeaturedBooksCubit(fetchFeatureBooksUseCase))
+            create: (_) => FeaturedBooksCubit(
+                FetchFeatureBooksUseCase(getIt.get<HomeRrpoImpl>()))),
+        BlocProvider(
+            create: (_) => NewestBooksCubit(
+                FetchNewestBooksUseCase(getIt.get<HomeRrpoImpl>()))),
       ],
       child: MaterialApp.router(
         routerConfig: AppRouter.router,
