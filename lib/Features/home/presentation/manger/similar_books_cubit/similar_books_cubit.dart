a30993c3b0fae16d3e44f14aff9588ba99290f18
Mohammed-bintaleb <engineerbintaleb@gmail.com>
2025-07-13 16:07:16 +0300

    simliar cubit

--- lib/Features/home/presentation/manger/similar_books_cubit/similar_books_cubit.dart
+++ lib/Features/home/presentation/manger/similar_books_cubit/similar_books_cubit.dart
@@ -1,8 +1,30 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../domain/entities/book_entity.dart';
+import '../../../domain/use_cases/fetch_similar_books_use_case.dart';
 
 part 'similar_books_state.dart';
 
 class SimilarBooksCubit extends Cubit<SimilarBooksState> {
-  SimilarBooksCubit() : super(SimilarBooksInitial());
+  SimilarBooksCubit(this.similarBooksUseCase) : super(SimilarBooksInitial());
+  final FetchSimilarBooksUseCase similarBooksUseCase;
+
+  Future<void> fetchSimilarBooks({int pageNumber = 0}) async {
+    if (pageNumber == 0) {
+      emit(SimilarBooksLoading());
+    } else {
+      emit(SimilarBooksPaginationLoading());
+    }
+
+    var result = await similarBooksUseCase.call(pageNumber);
+    result.fold((failure) {
+      if (pageNumber == 0) {
+        emit(SimilarBooksFailure(failure.message));
+      } else {
+        emit(SimilarBooksPaginationFailure(failure.message));
+      }
+    }, (books) {
+      emit(SimilarBooksSuccess(books));
+    });
+  }
 }
--- lib/Features/home/presentation/manger/similar_books_cubit/similar_books_state.dart
+++ lib/Features/home/presentation/manger/similar_books_cubit/similar_books_state.dart
@@ -1,6 +1,26 @@
 part of 'similar_books_cubit.dart';
 
-@immutable
-sealed class SimilarBooksState {}
+class SimilarBooksState {}
 
-final class SimilarBooksInitial extends SimilarBooksState {}
+class SimilarBooksInitial extends SimilarBooksState {}
+
+class SimilarBooksLoading extends SimilarBooksState {}
+
+class SimilarBooksSuccess extends SimilarBooksState {
+  final List<BookEntity> books;
+
+  SimilarBooksSuccess(this.books);
+}
+
+class SimilarBooksFailure extends SimilarBooksState {
+  final String errMessage;
+
+  SimilarBooksFailure(this.errMessage);
+}
+
+class SimilarBooksPaginationLoading extends SimilarBooksState {}
+
+class SimilarBooksPaginationFailure extends SimilarBooksState {
+  final String errMessage;
+  SimilarBooksPaginationFailure(this.errMessage);
+}
